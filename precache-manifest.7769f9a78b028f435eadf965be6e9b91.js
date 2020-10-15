c03ffc465033f45f4b9955bea6dbe85a65262c45
przekwas <luke@covalence.io>
2020-10-14 23:57:50 -0500

    Updates

---
+++ precache-manifest.7769f9a78b028f435eadf965be6e9b91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3356080eef955daab7ef1f84aa0a30",
+    "url": "/deadlands-character-builder/index.html"
+  },
+  {
+    "revision": "c43523c198ed0993f8e5",
+    "url": "/deadlands-character-builder/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "2076c3df0aa6da9b5cd2",
+    "url": "/deadlands-character-builder/static/css/main.eef8358e.chunk.css"
+  },
+  {
+    "revision": "c43523c198ed0993f8e5",
+    "url": "/deadlands-character-builder/static/js/2.a451225f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/deadlands-character-builder/static/js/2.a451225f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2076c3df0aa6da9b5cd2",
+    "url": "/deadlands-character-builder/static/js/main.17f2b792.chunk.js"
+  },
+  {
+    "revision": "2de4d423d5610712a366",
+    "url": "/deadlands-character-builder/static/js/runtime-main.c0a1e269.js"
+  }
+]);
